e380872d7ac599aa4ce0bbe1be4758d7c74347bf
caixiaobing <2380604178@qq.com>
2025-11-23 14:31:00 +0800

    039_左侧菜单静态搭建

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -4,7 +4,20 @@
         <div class="layout_slider">
             <Logo></Logo>
             <!-- 展示菜单 -->
+            <el-scrollbar class="scrollbar">
+                <el-menu background-color="#001529" text-color="white">
+                    <el-menu-item index="1">首页</el-menu-item>
+                    <el-menu-item index="2">数据大屏</el-menu-item>
+                    <!-- 折叠菜单 -->
+                    <el-sub-menu index="3">
+                        <template #title>商品管理</template>
+                        <el-menu-item index="3-1">商品列表</el-menu-item>
+                        <el-menu-item index="3-2">商品分类</el-menu-item>
+                    </el-sub-menu>
+                </el-menu>
+            </el-scrollbar>
         </div>
+
 
         <!-- 顶部导航 -->
         <div class="layout_tabbar">3245</div>
@@ -29,6 +42,11 @@ import Logo from './logo/index.vue'
         width: $base-menu-width;
         height: 100%;
         background: $base-menu-background;
+
+        .scrollbar {
+            width: 100%;
+            height: calc(100vh - #{$base-menu-logo-height});
+        }
     }
 
     .layout_tabbar {
